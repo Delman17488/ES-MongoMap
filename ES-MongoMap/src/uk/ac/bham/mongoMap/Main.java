df8fa417c168403a4d4c47ca64e7b8f674f6fcd0
odiehl <dxo445@student.bham.ac.uk>
2015-03-07 12:55:55 +0000

    SitraMapper implemented.

--- ES-MongoMap/src/uk/ac/bham/mongoMap/Main.java
+++ ES-MongoMap/src/uk/ac/bham/mongoMap/Main.java
@@ -1,10 +1,57 @@
 package uk.ac.bham.mongoMap;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import uk.ac.bham.mongoMap.map.SitraMapper;
+import uk.ac.bham.mongoMap.map.rules.CellToKeyVal;
+import uk.ac.bham.mongoMap.map.rules.ColumnToKey;
+import uk.ac.bham.mongoMap.map.rules.CompositePrimaryKeyToID;
+import uk.ac.bham.mongoMap.map.rules.DatabaseToMongoDB;
+import uk.ac.bham.mongoMap.map.rules.ForeignKeyToKeyVal;
+import uk.ac.bham.mongoMap.map.rules.PrimaryKeyToID;
+import uk.ac.bham.mongoMap.map.rules.RowToDocument;
+import uk.ac.bham.mongoMap.map.rules.TableToCollection;
+import uk.ac.bham.mongoMap.map.rules.UniqueToUniqueIndex;
+import uk.ac.bham.mongoMap.model.mongo.MongoDB;
+import uk.ac.bham.mongoMap.sql.SqlService;
+import uk.ac.bham.mongoMap.sql.SqlServiceImpl;
+import uk.ac.bham.sitra.Rule;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// add rules to list
+		List<Class<? extends Rule<?, ?>>> rules = new ArrayList<Class<? extends Rule<?, ?>>>();
+		rules.add(CellToKeyVal.class);
+		rules.add(ColumnToKey.class);
+		rules.add(CompositePrimaryKeyToID.class);
+		rules.add(DatabaseToMongoDB.class);
+		rules.add(ForeignKeyToKeyVal.class);
+		rules.add(PrimaryKeyToID.class);
+		rules.add(RowToDocument.class);
+		rules.add(TableToCollection.class);
+		rules.add(UniqueToUniqueIndex.class);
+
+		// instantiate sqlService and sitraMapper with rules
+		SitraMapper sitraMapper = new SitraMapper(rules);
+		SqlService service = new SqlServiceImpl();
+
+		// perform the transformation from SQL-DB to MongoDB
+		MongoDB mDB = null;
+		try {
+			mDB = (MongoDB) sitraMapper.performTransformation(service
+					.getDatabase(null));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// print the new and shiny mongoDB
+		if (mDB != null) {
+			System.out.println("MongoDB: ------");
+			System.out.println(mDB.toString());
+		}
 	}
-
 }
--- ES-MongoMap/src/uk/ac/bham/mongoMap/map/SitraMapper.java
+++ ES-MongoMap/src/uk/ac/bham/mongoMap/map/SitraMapper.java
@@ -1,7 +1,64 @@
 package uk.ac.bham.mongoMap.map;
 
-import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
+import java.util.List;
+
+import uk.ac.bham.mongoMap.model.mongo.Collection;
+import uk.ac.bham.mongoMap.model.mongo.Document;
+import uk.ac.bham.mongoMap.model.mongo.MongoDB;
+import uk.ac.bham.mongoMap.model.mongo.UniqueIndex;
+import uk.ac.bham.mongoMap.model.sql.Constraint;
+import uk.ac.bham.mongoMap.model.sql.Database;
+import uk.ac.bham.mongoMap.model.sql.Row;
+import uk.ac.bham.mongoMap.model.sql.Table;
+import uk.ac.bham.sitra.Rule;
+import uk.ac.bham.sitra.RuleNotFoundException;
+import uk.ac.bham.sitra.SimpleTransformerImpl;
+import uk.ac.bham.sitra.Transformer;
 
 public class SitraMapper {
-	
+
+	private Transformer transformer;
+
+	public SitraMapper(List<Class<? extends Rule<?, ?>>> rules) {
+		transformer = new SimpleTransformerImpl(rules);
+	}
+
+	public MongoDB performTransformation(Database db) throws Exception {
+		try {
+			// transform SQL-DB to MongoDB
+			MongoDB mongoDB = (MongoDB) transformer.transform(db);
+
+			for (Table table : db.getTable()) {
+				// transform table to collection
+				Collection coll = (Collection) transformer.transform(table);
+
+				// map unique constraints to unique indices and add them to the
+				// collection
+				for (Constraint constraint : table.getConstraints()) {
+					switch (constraint.getType()) {
+					case UNIQUE:
+						UniqueIndex index = (UniqueIndex) transformer
+								.transform(constraint);
+						coll.getUniqueIndices().add(index);
+						break;
+					default:
+						break;
+					}
+				}
+
+				// map rows to documents
+				for (Row row : table.getRows()) {
+					Document doc = (Document) transformer.transform(row);
+					coll.getDocuments().add(doc);
+				}
+
+				// add collection to mongoDB
+				mongoDB.getCollections().add(coll);
+			}
+
+			return mongoDB;
+		} catch (RuleNotFoundException e) {
+			throw new Exception("A rule could not have been found.", e);
+		}
+	}
 }
