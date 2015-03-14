d0761db8f6fe0c88aec627bbda08eb64376b9c6d
esubakarthik <subakarthik@192.168.0.3>
2015-03-14 12:21:31 +0000

    added status box to mongoDB write funtion

--- ES-MongoMap/src/uk/ac/bham/mongoMap/mongo/MongoServiceimpl.java
+++ ES-MongoMap/src/uk/ac/bham/mongoMap/mongo/MongoServiceimpl.java
@@ -1,7 +1,13 @@
 package uk.ac.bham.mongoMap.mongo;
 
+import java.awt.Component;
 import java.math.BigDecimal;
 import java.net.UnknownHostException;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 import org.eclipse.emf.common.util.EList;
 
@@ -36,14 +42,39 @@ public class MongoServiceimpl implements MongoService{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//open mongo database
+		long startTime = System.currentTimeMillis();
+
+		//open mongoDB database
 		String dbname= mDB.getName();
-		 db = mongo.getDB(dbname);
+		db = mongo.getDB(dbname);
+
+		JFrame frame = new JFrame("JOptionPane showMessageDialog ");
+		JLabel status = new JLabel();
+		final JOptionPane optionPane = new JOptionPane(
+				status
+				);
+
+		//create dialog box to show status
+		final JDialog dialog = new JDialog(frame, 
+				"Write Status",
+				true);
+		dialog.setSize(600, 110);
+		dialog.setContentPane(optionPane);
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				dialog.setVisible(true);
+			}
+		});
+		t.start();
+
+
+
+		int colcount=0;
 
 		for(Collection c : col)
 		{
 			String  colname= c.getName();
-
+			colcount++;
 			DBCollection collection= db.getCollection(colname);
 			collection.drop();
 			System.out.println("Collection : "+ colname);
@@ -55,7 +86,7 @@ public class MongoServiceimpl implements MongoService{
 				for (String key : list.getKeys()) {
 
 					System.out.println("unique index "+key);
-				//	collection.ensureIndex(new BasicDBObject(key, 1), new BasicDBObject("unique", true));
+					collection.ensureIndex(new BasicDBObject(key, 1), new BasicDBObject("unique", true));
 				}
 
 			}
@@ -63,32 +94,38 @@ public class MongoServiceimpl implements MongoService{
 			//add each document to the collection
 
 			EList<Document> doc = c.getDocuments();
+			int count=0;
 			for(Document document :doc)
 			{
-				
+				count++;
+				status.setText("<html>Working on Collection: "+colcount+"/"+col.size()+"<br>"+"Collection: "+ colname +" Documents added: "+count +"</html>");
+				status.repaint();
+
 				BasicDBObject documentObject=buildDocument(document);
 				//documentObject.
 				try{
-				collection.insert(documentObject);
+					collection.insert(documentObject);
 				}catch(IllegalArgumentException e){
-					
 					System.out.println("Illegal Argument");
-					
-					e.printStackTrace();
 				}
 
 			}
 
 			//display all documents in the collection
-			DBCursor cursorDocJSON = collection.find();
+			/*DBCursor cursorDocJSON = collection.find();
 			while (cursorDocJSON.hasNext()) {
 				System.out.println(cursorDocJSON.next());
 			}
-			System.out.println();
+			System.out.println();*/
 
 		}
 		// TODO Auto-generated method stub
+		dialog.setVisible(false);
+		long endTime = System.currentTimeMillis();
+		System.out.println("Time taken for writing data to mongoDB :"+((endTime-startTime)/1000)+" Seconds");
+		//System.exit(0);
 		return true;
+
 	}
 
 	public BasicDBObject buildDocument(Document document) {
@@ -111,6 +148,14 @@ public class MongoServiceimpl implements MongoService{
 				Value val = (Value) m;
 				Object value = val.getValue();
 
+				//check if given object datatype is supported for mongoDB
+				//value= checktype(value);
+				if(value instanceof BigDecimal )
+				{
+					BigDecimal bd=(BigDecimal) value;
+					Double d=bd.doubleValue();
+					value=d;
+				}
 				documentObject.put("_id", value);
 			}
 		}
@@ -130,23 +175,30 @@ public class MongoServiceimpl implements MongoService{
 		for(Key key:keys)
 		{
 			String name= key.getName();
-			
+
 			MongoObject m=	key.getValue();
 			if(m instanceof Value )
 			{
 				//if document
 				Value val = (Value) m;
 				Object value = val.getValue();
-				
+				if(value instanceof BigDecimal )
+				{
+					BigDecimal bd=(BigDecimal) value;
+					Double d=bd.doubleValue();
+					value=d;
+				}
+				//check if given object datatype is supported for mongoDB
 				//value= checktype(value);
-				
-												
+
+
 				documentObject.put(name, value);
 			}
 			else
 			{
 				//if value
 				BasicDBObject value= buildDocument((Document) m);
+
 				documentObject.put(name, value);
 			}
 		}
@@ -160,16 +212,13 @@ public class MongoServiceimpl implements MongoService{
 		BasicDBObject temp =  new BasicDBObject();
 		temp.put("sam", value);
 		try{
-		c.insert(temp);
+			c.insert(temp);
 		}catch(IllegalArgumentException e){
-			
-			//System.out.println("Illegal ");
-			BigDecimal tem=(BigDecimal) value;
-			
-			String t=tem.toString();
-			//e.printStackTrace();
-			//Double te= new Double(t);
-			value= t;
+
+			//change to string if non compatible
+			String tempstring = value.toString();			
+			value= tempstring;
+
 		}
 		return value;
 	}
